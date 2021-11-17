1051cf9060370ba7f03b87659ff9d84d0a38e67b
Sian Mancha <slmancha20@gmail.com>
2021-11-17 09:43:34 -0600

    Changed image to icon, added MoodCard, and added fetch

--- src/components/MoodBoard.js
+++ src/components/MoodBoard.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 import MoodForm from "./MoodForm";
+import MoodCard from "./MoodCard";
 
 function MoodBoard () {
+    const [moods, setMoods] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:6001/moods")
+        .then(res => res.json())
+        .then(data => setMoods(data))
+    }, [])
+    console.log(moods)
+
+    const displayMoods = moods.map(mood => <MoodCard key={mood.id } mood={mood} /> )
+
     return (
         <div>
             <h1>Mood Board</h1>
             <MoodForm />
+            {displayMoods}
             
             
         </div>
---
+++ src/components/MoodCard.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function MoodCard({mood: {id, name, icon, baseMood}}) {
+    
+    return (
+        <div>
+            Hi from Mood Card
+        </div>
+    )
+}
+
+export default MoodCard;
